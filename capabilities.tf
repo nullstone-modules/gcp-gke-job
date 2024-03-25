e0a285c512e5b855d38db8393fc1336efbcd3207
Brad Sickles <brad.sickles@gmail.com>
2024-03-25 17:45:24 -0400

    Initial setup

---
+++ capabilities.tf
@@ -0,0 +1,26 @@
+// This file is replaced by code-generation using 'capabilities.tf.tmpl'
+// This file helps app module creators define a contract for what types of capability outputs are supported.
+locals {
+  capabilities = {
+    // private_urls follows a wonky syntax so that we can send all capability outputs into the merge module
+    // Terraform requires that all members be of type list(map(any))
+    // They will be flattened into list(string) when we output from this module
+    private_urls = [
+      {
+        url = ""
+      }
+    ]
+
+    // public_urls follows a wonky syntax so that we can send all capability outputs into the merge module
+    // Terraform requires that all members be of type list(map(any))
+    // They will be flattened into list(string) when we output from this module
+    public_urls = [
+      {
+        url = ""
+      }
+    ]
+  }
+
+  cap_env_vars = {}
+  cap_secrets  = {}
+}
